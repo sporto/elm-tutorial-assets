de70618446a484e5b8346b1b675e7b05b2ef8f2a
Sebastian Porto <sebasporto@gmail.com>
2016-04-09 17:26:45 +1000

    Update BasicMouseDouble to use Html instead of Graphics

--- code/020_signals/BasicMouseDouble.elm
+++ code/020_signals/BasicMouseDouble.elm
@@ -1,16 +1,24 @@
 module Main (..) where
 
-import Graphics.Element exposing (..)
+import Html
 import Mouse
 
 
+double : Int -> Int
 double x =
   x * 2
 
 
+doubleSignal : Signal Int
 doubleSignal =
   Signal.map double Mouse.x
 
 
+view : Int -> Html.Html
+view x =
+  Html.text (toString x)
+
+
+main : Signal.Signal Html.Html
 main =
-  Signal.map show doubleSignal
+  Signal.map view doubleSignal
